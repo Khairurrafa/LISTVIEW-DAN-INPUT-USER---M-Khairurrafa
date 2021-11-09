aeaf26702227873f91e8f8c8c4da2510964aec45
Khairurrafa <erlangga.rafa022@gmail.com>
2021-11-09 13:30:52 +0700

    Latihan 2

---
+++ Latihan2/app/src/main/java/com/m/khairurrafa_rpl2/latihan2/Hero.java
@@ -0,0 +1,22 @@
+package com.m.khairurrafa_rpl2.latihan2;
+
+public class Hero {
+    int image;
+    String name, team;
+
+    public Hero(int image, String name, String team) {
+        this.image = image;
+        this.name = name;
+        this.team = team;
+    }
+    public int getImage() {
+        return image;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getTeam() {
+
+        return team;
+    }
+}
